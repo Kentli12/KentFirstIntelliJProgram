0ea262afc51b1f5ff86eeeaae1fe5b002417c0a9
Kent Li <kentl5@nycstudents.net>
2022-09-21 21:15:16 -0400

    added print statements

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -10,5 +10,6 @@ public class HelloWorld {
         System.out.println("I changed my code");
         System.out.println("Test Run 1");
         System.out.println("Test Run 2");
+        System.out.println("I sucessfully downloaded Intel");
     }
 }
